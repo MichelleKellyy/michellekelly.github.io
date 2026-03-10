c463962b94d316cb797fa79e9486ef3fcef99900
MichelleKellyy <michellebrookskelly@gmail.com>
2026-03-10 00:18:58 -0400

    added menu arrow key functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const items = document.querySelectorAll('.item');
 const output = document.getElementById('output');
+let activeItemIndex = 0;
 
 const content = {
     about: `
@@ -15,15 +16,32 @@ const content = {
 
 items.forEach(item => {
     item.addEventListener('click', () => {
-        items.forEach(i => i.classList.remove('active'));
-        item.classList.add('active');
-
+        setActive(item);
         showSection(item.dataset.section);
     });
 });
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowDown') {
+        if (activeItemIndex === items.length - 1) activeItemIndex = -1;
+        setActive(items[(activeItemIndex + 1)]);
+        showSection(items[activeItemIndex].dataset.section);
+    }
+    else if (event.key === 'ArrowUp') { 
+        if (activeItemIndex === 0) activeItemIndex = items.length;
+        setActive(items[(activeItemIndex - 1)]);
+        showSection(items[activeItemIndex].dataset.section);
+    }
+});
+
+function setActive(item) {
+    items.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+    activeItemIndex = [...items].indexOf(item);
+}
 
 function showSection(section) {
     output.innerText = content[section];
 }
 
-showSection("about");
+showSection('about');
