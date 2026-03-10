298ac289c60dd6d17073ca76c905007038a1b0c4
MichelleKellyy <michellebrookskelly@gmail.com>
2026-03-10 01:22:00 -0400

    Projects section + more styling + tweaked menu mechanics

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 const items = document.querySelectorAll('.item');
 const output = document.getElementById('output');
-let activeItemIndex = 0;
+let activeItemIndex = -1;
+let shownSectionIndex = -2;
 
 const content = {
     about: `
-      Hey there — glad you could make it, I'm Michelle.
-      I'm pretty into software engineering, AI and game development!
+      <p>
+      Hey there — glad you could make it, I'm Michelle. <br>
+      I'm pretty into <b>software engineering, AI and game development!</b>
+      </p>
+      
+    `,
+    projects: `
+      <p>
+      Here are some of my recent projects:
+      </p>
+      <ul>
+        <li><a href="https://github.com/MichelleKellyy/ConnectKingston" target="_blank">ConnectKingston:</a> An AI-powered matching platform that helps Kingston residents discover meaningful volunteer opportunities tailored to their interests and skills. (add website)</li>
+        <li><a href="https://github.com/MichelleKellyy/DAIR-Music-Genre-Transfer" target="_blank">Music Genre Transfer:</a> Instrument-specific symbolic music genre transfer with machine learning. (add paper)</li>
+        <li><a href="https://github.com/MichelleKellyy/FaceShield" target="_blank">FaceShield:</a> An app that analyzes how vulnerable a face photo is to AI recognition, cloaks it using near invisible privacy-preserving image transformations with before-and-after protection scoring. (add demo + doc)</li>
+      </ul>
     `,
     contact: `
-      Reach out! Always open to new opportunities and collaborations.
-      michelle.kelly@queensu.ca
+      <p>
+      Reach out! Always open to new opportunities and collaborations. <br> <br>
+      <a href="mailto:michelle.kelly@queensu.ca">michelle.kelly@queensu.ca</a>
+      </p>
     `
 };
 
-
 items.forEach(item => {
     item.addEventListener('click', () => {
-        setActive(item);
-        showSection(item.dataset.section);
+        if ([...items].indexOf(item) === shownSectionIndex) {
+            output.innerHTML = '';
+            shownSectionIndex = -2;
+        }
+        else {
+            setActive(item);
+            showSection(item.dataset.section);
+        }
     });
 });
 
@@ -25,12 +46,19 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowDown') {
         if (activeItemIndex === items.length - 1) activeItemIndex = -1;
         setActive(items[(activeItemIndex + 1)]);
-        showSection(items[activeItemIndex].dataset.section);
     }
     else if (event.key === 'ArrowUp') { 
         if (activeItemIndex === 0) activeItemIndex = items.length;
         setActive(items[(activeItemIndex - 1)]);
-        showSection(items[activeItemIndex].dataset.section);
+    }
+    else if (event.key === 'Enter') {
+        if (activeItemIndex === shownSectionIndex) {
+            output.innerHTML = '';
+            shownSectionIndex = -2;
+        }
+        else {
+            showSection(items[activeItemIndex].dataset.section);
+        }
     }
 });
 
@@ -41,7 +69,6 @@ function setActive(item) {
 }
 
 function showSection(section) {
-    output.innerText = content[section];
+    output.innerHTML = content[section];
+    shownSectionIndex = activeItemIndex;
 }
-
-showSection('about');
