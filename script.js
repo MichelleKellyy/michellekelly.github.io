5907490254128b962847eba6f88892a9f92d1a72
MichelleKellyy <michellebrookskelly@gmail.com>
2026-03-24 16:42:25 -0400

    Add simple modal for proj cards

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ window.addEventListener('scroll', function() {
     nav.style.transform = past ? 'translateY(0)' : 'translateY(-100%)';
     nav.style.pointerEvents = past ? 'all' : 'none';
 });
+
+const openModal = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
